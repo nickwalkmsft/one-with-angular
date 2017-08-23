0b58981d301423cf5bc93b69a648442d533b9b67
Shayne Boyer <spboyer@live.com>
2017-08-23 16:01:21 -0400

    change root path

--- src/server/index.js
+++ src/server/index.js
@@ -8,10 +8,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(root, 'dist')));
+app.use(express.static(root));
 // app.use('/api', routes);
 app.get('*', (req, res) => {
-  res.sendFile('dist/index.html', {root: root});
+  res.sendFile('index.html', {root: root});
 });
 
 const port = process.env.PORT || '3000';
